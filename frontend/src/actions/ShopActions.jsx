ade949385db55e5d256ee442f0824f73c2bf9385
Coral Solomon <coralsolomon179@gmail.com>
2020-01-15 18:33:19 +0200

    coral-1833

--- frontend/src/actions/ShopActions.jsx
+++ frontend/src/actions/ShopActions.jsx
@@ -1,4 +1,19 @@
+import ShopService from '../services/ShopService'
 
-// export function XXX(){
-    
-// }
+export function loadItems() {
+    return async dispatch => {
+        try {
+            const items = await ShopService.query();
+            dispatch(setItems(items));
+        } catch (err) {
+            console.log('ShopActions: err in loadItems', err);
+        }
+    }
+}
+
+function setItems(items) {
+    return {
+        type: 'SET_ITEMS',
+        items
+    }
+}
