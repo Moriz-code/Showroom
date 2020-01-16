da55c80b4181dc713a3d379b536d20a32ebf12fe
Coral Solomon <coralsolomon179@gmail.com>
2020-01-16 14:38:39 +0200

    cori

--- frontend/src/cmps/items/ItemList.jsx
+++ frontend/src/cmps/items/ItemList.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 import ItemPreview from '../items/ItemPreview'
 import { withRouter } from "react-router";
 
-function ItemList(items) {
-  console.log('itemsList', items.items);
-  let itemsToShow = items.items
-  return <ul>{itemsToShow.length > 0 ? itemsToShow.map(item => <ItemPreview key={item._id} item={item}></ItemPreview>) : 'No items'}</ul>
+function ItemList({items}) {
+  return <ul className="cards-container">{items.length > 0 ? items.map(item => <ItemPreview key={item._id} item={item}></ItemPreview>) : 'No items'}</ul>
 }
 export default withRouter(ItemList)
--- frontend/src/cmps/items/ItemPreview.jsx
+++ frontend/src/cmps/items/ItemPreview.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export default function ItemPreview({item}) {
   return (<Link to={`/item/${item.id}`}>
-    <li>
+    <li className="item-card clean-line">
       <img src={item.imgs[0]} />
       <h3>{item.title}</h3>
       <p>{item.price}</p>
