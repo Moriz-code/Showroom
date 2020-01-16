49d9d1b2b2d8b2a0853aa93565aa14c1753c1a8b
moriz-code <morzavi1@gmail.com>
2020-01-16 13:05:29 +0200

    morrrrr

--- frontend/src/cmps/Header.jsx
+++ frontend/src/cmps/Header.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
+import {Link} from 'react-router-dom'
 
 
 class Header extends Component {
   render() {
     return <React.Fragment>
       <div className="main-header flex">
-        <div className="logo">LOGO</div>
+     <Link to={`/`}><div className="logo">LOGO</div></Link> 
         <NavBar></NavBar>
       </div>
     </React.Fragment>
