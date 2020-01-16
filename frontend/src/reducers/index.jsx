f90a55f9c75075c49b5804426c3f937dcaf76aad
moriz-code <morzavi1@gmail.com>
2020-01-16 12:24:48 +0200

    opop

--- frontend/src/reducers/index.jsx
+++ frontend/src/reducers/index.jsx
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import OrderReducer from './OrderReducer';
 import ShopReducer from './ShopReducer';
 import UserReducer from './UserReducer';
+import ItemReducer from './ItemReducer';
 
 const rootReducer = combineReducers({
-    order:OrderReducer,
+    item: ItemReducer,
+    order: OrderReducer,
     shop: ShopReducer,
     user: UserReducer
 })
